749dbc7579983390037fe7d38a5a000f4207f673
AJ-17 <31892477+AJ-17@users.noreply.github.com>
2017-12-05 16:52:16 +0100

    Update gravitation.py

--- gravitation.py
+++ gravitation.py
@@ -11,7 +11,7 @@ def entrées():
 #Cette fonction calcul la force de gravitation selon la distance et la masse des deux objets
 #entrées : a,b,masse1 et masse2 sont des nombre réels
 #sortie : la fonction retourne le réel somme
-def metier(a,b,masse1,masse2):
+def metier(a,masse1,masse2):
     gravitation = float((6.67*10**(-11))*((masse1*masse2)/(a**2)))
     return gravitation
 
@@ -24,7 +24,7 @@ def affiche(gravitation,a):
 
 a,b,masse1,masse2=entrées()
 while(a<b):
-    gravitation = metier(a,b,masse1,masse2)
+    gravitation = metier(a,masse1,masse2)
     affiche(gravitation,a)
     a=a*2
 
